42f6d5c417ddcc9368299ad209ca61053d4939b1
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2020-08-13 13:02:56 -0500

    added Projects and routing

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import Projects from './ProjectsComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 
 class Main extends Component {
 
     render(){
         return(
             <div>
-                <Home></Home>
+                <Switch>
+                    <Route path="/home" component={() => <Home/>}/>
+                    <Route exact path="/projects" component={() => <Projects/>}/>
+                    <Redirect to="/home"/>
+                </Switch>
             </div>
         );
     }
 }
-export default Main;
+export default withRouter(Main);
