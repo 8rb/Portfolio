3957538f4573dca7fc67cea405ff93690d88c96e
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2020-12-24 22:25:02 -0500

    🔨 Changed Projects to a List
    Changed individual Projects Components to a map of projects using an external js array file.

---
+++ src/json/projects.js
@@ -0,0 +1,50 @@
+export const projects = [
+    {
+        "id": 1,
+        "title": "PokeApp",
+        "description": "PokeApp is a Pokemon Encyclopedia and type calculator developed in React + TypeScript. It fetches data from an external API (PokeAPI) using Axios. It also takes advantage of React Hooks, Function Components, Use Effect, etc. The prototyping was made using Adobe XD and the views were made using React Bootstrap.",
+        "github_link": "https://github.com/8rb/PokeApp",
+        "deployed_link": "https://pokeapp.onrender.com/",
+        "img": require("../images/pokeffective.png"),
+        "img_classname": "pokeball logo iconCool"
+    },
+    {
+        "id": 2,
+        "title": "FinanceeApp",
+        "description": "FinanceeApp is an online shopping website, based on Peruvian 'Bodegas'. It was developed in Vue and Typescript, and uses bootstrap and SCSS for the UI elements. This project uses Vuex store and takes advantage of Axios to make HTTP requests to the Server.",
+        "github_link": "https://github.com/8rb/FinanciApp-FrontEnd",
+        "deployed_link": "https://financeeapp.onrender.com",
+        "img": require("../images/FinanceeApp.png"),
+        "img_classname": "roundedLogo rounded-circle logo iconCool"
+    },
+    {
+        "id": 3,
+        "title": "InterLab",
+        "description": "Interlab is a LinkedIn clone, developed in Vue.js in the Front-End and .Net Core in the backend. I was in charge of the Majority of the Front-End programming and the database design. This application accesses the backend using Axios for HTTP requests.",
+        "github_link": "https://github.com/8rb/interlab-webapps",
+        "deployed_link": "https://interlab.onrender.com/#/",
+        "img": require("../images/interlab.png"),
+        "img_classname": "roundedLogo rounded-circle logo iconCool"
+    },
+        
+        // {
+        //     "id": 4,
+        //     "title": "Algorithm Visualizer",
+        //     "description": "",
+        //     "github_link": "",
+        //     "deployed_link": "",
+        //     "img": "",
+        //     "img_classname": ""
+        // },
+        // {
+        //     "id": 5,
+        //     "title": "Useless App",
+        //     "description": "",
+        //     "github_link": "",
+        //     "deployed_link": "",
+        //     "img": "",
+        //     "img_classname": ""
+        // }
+    ]
+
+export default projects;
