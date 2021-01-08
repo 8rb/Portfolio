7d29c02a32cfdf5acf36f492472be585b6ca6830
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-01-08 00:17:26 -0500

    ⚡ Updated projects
    Updated Bodega from FinanceeApp and changed projects descriptions.

--- src/json/projects.js
+++ src/json/projects.js
@@ -2,7 +2,7 @@ export const projects = [
     {
         "id": 1,
         "title": "PokeApp",
-        "description": "PokeApp is a Pokemon Encyclopedia and type calculator developed in React + TypeScript. It fetches data from an external API (PokeAPI) using Axios. It also takes advantage of React Hooks, Function Components, Use Effect, etc. The prototyping was made using Adobe XD and the views were made using React Bootstrap.",
+        "description": "PokeApp is a Pokemon Search Engine and type calculator developed in React + TypeScript. It fetches data from an external API (PokeAPI) using Axios and filters the most accurate results based on the user input. It also takes advantage of React Hooks, Function Components, Use Effect, etc. The prototyping was made using Adobe XD and the views were made using Bootstrap and CSS.",
         "github_link": "https://github.com/8rb/PokeApp",
         "deployed_link": "https://pokeapp.onrender.com/",
         "img": require("../images/pokeapp.png"),
@@ -11,18 +11,18 @@ export const projects = [
     },
     {
         "id": 2,
-        "title": "FinanceeApp",
-        "description": "FinanceeApp is an online shopping website, based on Peruvian 'Bodegas'. It was developed in Vue and Typescript, and uses bootstrap and SCSS for the UI elements. This project uses Vuex store and takes advantage of Axios to make HTTP requests to the Server.",
-        "github_link": "https://github.com/8rb/FinanciApp-FrontEnd",
-        "deployed_link": "https://financeeapp.onrender.com",
-        "img": require("../images/FinanceeApp.png"),
+        "title": "Bodega",
+        "description": "Bodega is an online shopping website, based on Peruvian 'Bodegas'. It was developed in Vue and Typescript, and uses Bootstrap and SCSS for the UI elements. This project uses Vuex store to store the state of the cart when rendering the component.",
+        "github_link": "https://github.com/8rb/Bodega-FrontEnd",
+        "deployed_link": "https://bodega.onrender.com",
+        "img": require("../images/Bodega.png"),
         "img_classname": "logo iconCool",
         "row_classname": "align-items-center justify-content-center pt-0 pt-sm-4 pb-0 pb-sm-4"
     },
     {
         "id": 3,
         "title": "InterLab",
-        "description": "Interlab is a LinkedIn clone, developed in Vue.js in the Front-End and .Net Core in the backend. I was in charge of the Majority of the Front-End programming and the database design. This application accesses the backend using Axios for HTTP requests.",
+        "description": "Interlab is an internship applications platform, developed in Vue.js in the Front-End and .Net Core in the backend. I was in charge of the Majority of the Front-End programming and the database design. This application accesses the backend using Axios for HTTP requests.",
         "github_link": "https://github.com/8rb/interlab-webapps",
         "deployed_link": "https://interlab.onrender.com/#/",
         "img": require("../images/interlab.png"),
