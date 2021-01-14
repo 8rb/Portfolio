7d67dbd1060ad6fcaa2d07d755db63171d12ef32
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-01-13 23:09:00 -0500

    🔗 Updated PokeApp Links
    Updated Projects json links (PokeApp).

--- src/json/projects.js
+++ src/json/projects.js
@@ -4,7 +4,7 @@ export const projects = [
         "title": "PokeApp",
         "description": "PokeApp is a Pokemon Search Engine and type calculator developed in React + TypeScript. It fetches data from an external API (PokeAPI) using Axios and filters the most accurate results based on the user input. It also takes advantage of React Hooks, Function Components, Use Effect, etc. The prototyping was made using Adobe XD and the views were made using Bootstrap and CSS.",
         "github_link": "https://github.com/8rb/PokeApp",
-        "deployed_link": "https://pokeapp.onrender.com/",
+        "deployed_link": "https://pokeapp.onrender.com/search",
         "img": require("../images/pokeapp.png"),
         "img_classname": "logo iconCool",
         "row_classname": "align-items-center justify-content-center pt-0 pt-sm-4 pb-0 pb-sm-4"
@@ -24,7 +24,7 @@ export const projects = [
         "title": "InterLab",
         "description": "Interlab is an internship applications platform, developed in Vue.js in the Front-End and .Net Core in the backend. I was in charge of the Majority of the Front-End programming and the database design. This application accesses the backend using Axios for HTTP requests.",
         "github_link": "https://github.com/8rb/interlab-webapps",
-        "deployed_link": "https://interlab.onrender.com/#/",
+        "deployed_link": "https://interlab.onrender.com/",
         "img": require("../images/interlab.png"),
         "img_classname": "logo iconCool",
         "row_classname": "align-items-center justify-content-center pt-0 pt-sm-4"
