4c4d48f305b51d0d106e7029ef8e87c35e52d865
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-01-17 19:21:58 -0500

    ✨ Created NavBar
    Created Navbar.

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Pdf from '../../Documents/Rodrigo_Ramirez_Resume_2021.pdf';
+import Pdf from '../../documents/Rodrigo_Ramirez_Resume_2021.pdf';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import {Spring} from 'react-spring/renderprops';
