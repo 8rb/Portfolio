0a267ab8ca6ca10573c9cd8cefd395e426ee6cca
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-01-17 19:21:14 -0500

    🎨 Improved Home
    Changed Colors and fonts of Home Component.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -45,43 +45,45 @@ const Home = () => {
 
     return(
         <div className="home">
-            {/* <Row className="align-items-center toggle-height">
-                <Col xs={12}>
-                    <Row>
-                        <Spring
-                            from={{marginTop: -100 }}
-                            to={{marginTop: 0 }}
-                            config={{delay: 0, duration: 600}}
-                        >
-                            { props => (
-                        <div style={props}>
-                            <Toggle/>
-                        </div>
-                            )}
-                        </Spring>
-                    </Row>
-                </Col>
-            </Row> */}
-            <Row className="align-items-center home-height">
-                <Col xs={12}>
-                    <Row className="justify-content-center">
-                        <Col xs={12}>
-                            <animated.div style={props}>
-                                <h1 className="centered-text name">Rodrigo Ramirez</h1>
-                            </animated.div>
-                        </Col>
-                    </Row>
-                    <Row className="justify-content-center">
-                        <Col xs={12}>
-                            <animated.div style={props}>
-                                <h2 className="centered-text job">Front-End Developer</h2>
-                            </animated.div>
-                        </Col>
-                    </Row>
-                    <Contact/>
-                </Col>
-            </Row>
-            {/* <Footer/> */}
+            <div className="container-fluid">
+                {/* <Row className="align-items-center toggle-height">
+                    <Col xs={12}>
+                        <Row>
+                            <Spring
+                                from={{marginTop: -100 }}
+                                to={{marginTop: 0 }}
+                                config={{delay: 0, duration: 600}}
+                            >
+                                { props => (
+                            <div style={props}>
+                                <Toggle/>
+                            </div>
+                                )}
+                            </Spring>
+                        </Row>
+                    </Col>
+                </Row> */}
+                <Row className="align-items-center home-height">
+                    <Col xs={12}>
+                        <Row className="justify-content-center">
+                            <Col xs={12}>
+                                <animated.div style={props}>
+                                    <h1 className="centered-text name">Rodrigo Ramirez</h1>
+                                </animated.div>
+                            </Col>
+                        </Row>
+                        <Row className="justify-content-center">
+                            <Col xs={12}>
+                                <animated.div style={props}>
+                                    <h2 className="centered-text job">Front-End Developer</h2>
+                                </animated.div>
+                            </Col>
+                        </Row>
+                        <Contact/>
+                    </Col>
+                </Row>
+                {/* <Footer/> */}
+            </div>
         </div>
     ); 
 
