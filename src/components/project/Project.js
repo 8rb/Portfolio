1f7c243db371403053887de4f448dca4fb3d3a32
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-01-21 10:52:43 -0500

    🔨 Refactored Project Component
    Changed Project Component Structure to Flexbox CSS.

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -6,54 +6,34 @@ import './Styles.scss';
 
 const Project = ({index}) => {
   return (
-    <>
-    <Row className="align-items-center project-div">
-      <Col xs="auto">
-        <Row className="align-items-center">
-          <Col xs={12} sm={6}>
-            <Row className="justify-content-center">
-              <Col xs="auto">
-                <img className="imageP" src={projectsInfo[index].img} alt="project"/>
-              </Col>
-            </Row>
-          </Col>
-          <Col xs={12} sm={6}>
-            <Row className="justify-content-start">
-              <Col xs={12}>
-                <h1 className="titleP">{projectsInfo[index].title}</h1>
-              </Col>
-            </Row>
-            <Row className="justify-content-start">
-              <Col>
-                {projectsInfo[index].tags.map((tag, i) =>
-                  <div className="tag-div" key={i}><p className="tag">{tag}</p></div>
-                )}
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={12}>
-                <div className="text-div">
-                  <p className="text">{projectsInfo[index].description}</p>
-                </div>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-    <Row className="align-items-center links-div">
-      <Col xs={12}>
-        <Row className="align-items-center">
-          <Col xs={12} sm={6}>
-            <a className="btn github" href={projectsInfo[index].github_link} target="_blank" rel="noopener noreferrer">Github Repo</a>
-          </Col>
-          <Col xs={12} sm={6}>
-            <a className="btn live" href={projectsInfo[index].deployed_link} target="_blank" rel="noopener noreferrer">Live Site</a>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-    </>
+    <div className="pro-container">
+    <div className="pro-row">
+      <div className="pro-col">
+        <img className="imageP" src={projectsInfo[index].img} alt="project"/>
+      </div>
+      <div className="pro-col">
+        <div className="title-row">
+          <h1 className="titleP">{projectsInfo[index].title}</h1>
+        </div>
+        <div className="tags-row">
+          {projectsInfo[index].tags.map((tag, i) =>
+            <div className="tag-div" key={i}><p className="tag">{tag}</p></div>
+          )}
+        </div>
+        <div className="desc-row">
+          <p className="text">{projectsInfo[index].description}</p>
+        </div>
+      </div>
+    </div>
+    <div className="links-row">
+      <div className="link-col">
+        <a className="btn github" href={projectsInfo[index].github_link} target="_blank" rel="noopener noreferrer">Github Repo</a>
+      </div>
+      <div className="link-col">
+        <a className="btn live" href={projectsInfo[index].deployed_link} target="_blank" rel="noopener noreferrer">Live Site</a>
+      </div>
+    </div>
+    </div>
   );
 }
 
