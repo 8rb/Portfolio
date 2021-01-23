4d65b92cd08e8040f9779ee9d816d4f681d916bc
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-01-22 23:02:07 -0500

    🔨 Refactored Home Component
    Refactored Home to flexbox. Removed redundant styles.

--- src/App.js
+++ src/App.js
@@ -8,24 +8,28 @@ class App extends Component {
   
   render() {
     return(
-      <Router>
-        <NavBar/>
-        <div className="theme content">
-          <Switch>
-            <Route exact path="/">
-                <Home/>
-            </Route>
-            <Route path="/projects">
-                {/* <Project/> */}
-                <AllProjects/>
-                {/* <Projects/> */}
-            </Route>
-            <Route path="*">
-                <Redirect to="/"/>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <div className="theme">
+        <Router>
+          <div className="navbar-content">
+            <NavBar/>
+          </div>
+          <div className="main-content">
+            <Switch>
+              <Route exact path="/">
+                  <Home/>
+              </Route>
+              <Route path="/projects">
+                  {/* <Project/> */}
+                  <AllProjects/>
+                  {/* <Projects/> */}
+              </Route>
+              <Route path="*">
+                  <Redirect to="/"/>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
     )
   }
 }
