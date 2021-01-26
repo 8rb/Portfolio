04a36201d7894dec45db4df9776a98e88f5a10cd
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-01-26 15:53:03 -0500

    ⚡ Updated Main App
    Updated css and scss from index.js and app.js, removed unused assets and changed manifest.json.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavBar from './components/navBar/NavBar';
 import Home from './components/home/Home';
-import AllProjects from './components/projectsList/AllProjects';
-import './App.scss';
+import Projects from './components/projects/Projects';
 import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
-class App extends Component {
-  
-  render() {
-    return(
-      <div className="theme">
-        <Router>
-          <div className="navbar-content">
-            <NavBar/>
-          </div>
-          <div className="main-content">
-            <Switch>
-              <Route exact path="/">
-                  <Home/>
-              </Route>
-              <Route path="/projects">
-                  {/* <Project/> */}
-                  <AllProjects/>
-                  {/* <Projects/> */}
-              </Route>
-              <Route path="*">
-                  <Redirect to="/"/>
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    )
-  }
+
+const App = () => {
+
+  return(
+    <div className="theme">
+      <Router>
+        <div className="navbar-content">
+          <NavBar/>
+        </div>
+        <div className="main-content">
+          <Switch>
+            <Route exact path="/">
+                <Home/>
+            </Route>
+            <Route path="/projects">
+                <Projects/>
+            </Route>
+            <Route path="*">
+                <Redirect to="/"/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
