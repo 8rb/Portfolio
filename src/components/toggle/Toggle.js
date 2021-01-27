60314411c2572f50738e97c6334d378fc24f572f
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-01-27 11:56:06 -0500

    ✨ Dark Mode Toggle
    Re-Implemented Dark Mode toggle using react hooks.

--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -1,65 +1,52 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Styles.scss';
-class ComponentWrapper extends React.Component {
+
+const Toggle = () => {
+	const [isOn, setIsOn] = React.useState(false);
+
+	useEffect(() => {
+		let dataColorMode = document.getElementById('theme').getAttribute('data-color-mode');
+		if(dataColorMode === "light") {
+			setIsOn(false);
+		}
+		if(dataColorMode === "dark") {
+			setIsOn(true);
+		}
+	}, []);
 	
-	constructor(props) {
-		super(props);
-		this.state = {
-			isOn: null
-		};
-		let MY_SALVATION = document.getElementById('darkHtml').getAttribute('data-theme');
-		if(MY_SALVATION === "") {
-			this.state = {
-				isOn: false
-			};
+	const handleToggle = (e) => {
+		let dataColorMode = document.getElementById('theme').getAttribute('data-color-mode');
+		if(dataColorMode === "light") {
+			document.getElementById('theme').setAttribute('data-color-mode', 'dark');
 		}
-		if(MY_SALVATION === "dark") {
-			this.state = {
-				isOn: true
-			};
+		if(dataColorMode === "dark") {
+			document.getElementById('theme').setAttribute('data-color-mode', 'light');
 		}
+		setIsOn(!isOn);
 	}
 	
-	handleToggle(e) {
-		let MY_SALVATION = document.getElementById('darkHtml').getAttribute('data-theme');
-		if(MY_SALVATION === "") {
-			this.setState({isOn: true});
-			document.getElementById('darkHtml').setAttribute('data-theme', 'dark');
-		}
-		if(MY_SALVATION === "dark") {
-			this.setState({isOn: false});
-			document.getElementById('darkHtml').setAttribute('data-theme', '');
-		}
-		this.setState({isOn: !this.state.isOn});
-	}
-	
-	render() {
-		let classNames = ["component-wrapper", (this.state.isOn) ? "component-wrapper_is-light" : "component-wrapper_is-dark"].join(" ");
-		return (
-			<div className={classNames}>
-				<Switch 
-					isOn={this.state.isOn}	
-					handleToggle={this.handleToggle.bind(this)}
-				/>
-			</div>		
-		);
-	}
+	let classNames = ["", (isOn) ? "component-wrapper_is-light" : "component-wrapper_is-dark"].join(" ");
+	return (
+		<div className={classNames}>
+			<Switch isOn={isOn} handleToggle={handleToggle}/>
+		</div>	
+	);
 }
 
-const Switch = function(props) {
+const Switch = (props) => {
 	let classNames = ["switch", (props.isOn) ? "switch_is-on" : "switch_is-off"].join(" ");
 	return (
 		<div className={classNames} onClick={props.handleToggle}>
-			<ToggleButton 
-				isOn={props.isOn}	
-			/>
+			<ToggleButton isOn={props.isOn}	/>
 		</div>
 	);
 }
 
-const ToggleButton = function(props) {
-		let classNames = ["", (props.isOn) ? "toggle-button-on toggle-button_position-right" : "toggle-button toggle-button_position-left"].join(" ");
-		return (<div className={classNames}></div>);
+const ToggleButton = (props) => {
+	let classNames = [(props.isOn) ? "toggle-button-on toggle-button_position-right" : "toggle-button toggle-button_position-left"];
+	return (
+	<div className={classNames}></div>
+	);
 };
 
-export default ComponentWrapper;
+export default Toggle;
