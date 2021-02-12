5b3a74feaefa393a3e13ffa953f56e64fe00f84c
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-02-11 21:29:29 -0500

    🔨 Refactored Contact
    Refactored Contact Component

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -23,80 +23,82 @@ const Contact = () => {
     }
     
     return (
-        <div className="contactContainer contact">
-            <div className="contactRow">
-                <div className="contactCol iconCol">
-                    <Spring
-                        from={{opacity: 0 }}
-                        to={{opacity: 1 }}
-                        config={{delay: 500, duration: 500}}
-                        >
-                        { props => (
-                        <a className="contactLink" style={props} onMouseEnter={toggleResume} onMouseLeave={toggleResume} href={Pdf} target ="_blank" rel="noopener noreferrer">
-                            <i className="homeIcon fas fa-file-pdf fa-2x"></i>
-                        </a>
-                        )}
-                    </Spring>
+        <div className="contact">
+            <div className="contactContainer">
+                <div className="contactRow">
+                    <div className="contactCol iconCol">
+                        <Spring
+                            from={{opacity: 0 }}
+                            to={{opacity: 1 }}
+                            config={{delay: 500, duration: 500}}
+                            >
+                            { props => (
+                            <a className="contactLink" style={props} onMouseEnter={toggleResume} onMouseLeave={toggleResume} href={Pdf} target ="_blank" rel="noopener noreferrer">
+                                <i className="homeIcon fas fa-file-pdf fa-2x"></i>
+                            </a>
+                            )}
+                        </Spring>
+                    </div>
+                    <div className="contactCol iconCol">
+                        <Spring
+                            from={{opacity: 0 }}
+                            to={{opacity: 1 }}
+                            config={{delay: 700, duration: 500}}
+                            >
+                            { props => (
+                            <a className="contactLink" style={props} onMouseEnter={toggleGithub} onMouseLeave={toggleGithub} href="https://github.com/8rb" target="_blank" rel="noopener noreferrer">
+                                <i className="homeIcon fab fa-github fa-2x"></i>
+                            </a>
+                            )}
+                        </Spring>
+                    </div>
+                    <div className="contactCol iconCol">
+                        <Spring
+                            from={{opacity: 0 }}
+                            to={{opacity: 1 }}
+                            config={{delay: 900, duration: 500}}
+                            >
+                            { props => (
+                            <a className="contactLink" style={props} onMouseEnter={toggleLinkedin} onMouseLeave={toggleLinkedin} href="https://www.linkedin.com/in/rodrigo-ramirez8/" target="_blank" rel="noopener noreferrer">
+                                <i className="homeIcon fab fa-linkedin-in fa-2x"></i>
+                            </a>
+                            )}
+                        </Spring>
+                    </div>
+                    <div className="contactCol iconCol">
+                        <Spring
+                            from={{opacity: 0 }}
+                            to={{opacity: 1 }}
+                            config={{delay: 1100, duration: 500}}
+                            >
+                            { props => (
+                            <a className="contactLink" style={props} onMouseEnter={toggleEmail} onMouseLeave={toggleEmail} href="mailto:rodrigo8rb@hotmail.com">
+                                <i className="homeIcon fas fa-envelope fa-2x"></i>
+                            </a>
+                            )}
+                        </Spring>
+                    </div>
                 </div>
-                <div className="contactCol iconCol">
-                    <Spring
-                        from={{opacity: 0 }}
-                        to={{opacity: 1 }}
-                        config={{delay: 700, duration: 500}}
-                        >
-                        { props => (
-                        <a className="contactLink" style={props} onMouseEnter={toggleGithub} onMouseLeave={toggleGithub} href="https://github.com/8rb" target="_blank" rel="noopener noreferrer">
-                            <i className="homeIcon fab fa-github fa-2x"></i>
-                        </a>
+                <div className="contactRow">
+                    <div className="contactCol">
+                        <Spring
+                            from={{opacity: 0 }}
+                            to={{opacity: 1 }}
+                            config={{delay: 1300, duration: 500}}
+                            >
+                            { props => (
+                            <div className="contactCol" style={props}>
+                                { resume && <p className="contactText">Resume</p>}
+                                { github && <p className="contactText">GitHub</p>}
+                                { linkedin && <p className="contactText">LinkedIn</p>}
+                                { email && <p className="contactText">Email Me</p>}
+                                { !resume && !github && !linkedin && !email &&
+                                    <p className="contactText">{'Contact'}</p>
+                                }
+                            </div>
                         )}
-                    </Spring>
-                </div>
-                <div className="contactCol iconCol">
-                    <Spring
-                        from={{opacity: 0 }}
-                        to={{opacity: 1 }}
-                        config={{delay: 900, duration: 500}}
-                        >
-                        { props => (
-                        <a className="contactLink" style={props} onMouseEnter={toggleLinkedin} onMouseLeave={toggleLinkedin} href="https://www.linkedin.com/in/rodrigo-ramirez8/" target="_blank" rel="noopener noreferrer">
-                            <i className="homeIcon fab fa-linkedin-in fa-2x"></i>
-                        </a>
-                        )}
-                    </Spring>
-                </div>
-                <div className="contactCol iconCol">
-                    <Spring
-                        from={{opacity: 0 }}
-                        to={{opacity: 1 }}
-                        config={{delay: 1100, duration: 500}}
-                        >
-                        { props => (
-                        <a className="contactLink" style={props} onMouseEnter={toggleEmail} onMouseLeave={toggleEmail} href="mailto:rodrigo8rb@hotmail.com">
-                            <i className="homeIcon fas fa-envelope fa-2x"></i>
-                        </a>
-                        )}
-                    </Spring>
-                </div>
-            </div>
-            <div className="contactRow">
-                <div className="contactCol">
-                    <Spring
-                        from={{opacity: 0 }}
-                        to={{opacity: 1 }}
-                        config={{delay: 1300, duration: 500}}
-                        >
-                        { props => (
-                        <div className="contactCol" style={props}>
-                            { resume && <p className="contactText">Resume</p>}
-                            { github && <p className="contactText">GitHub</p>}
-                            { linkedin && <p className="contactText">LinkedIn</p>}
-                            { email && <p className="contactText">Email Me</p>}
-                            { !resume && !github && !linkedin && !email &&
-                                <p className="contactText">{'Contact'}</p>
-                            }
-                        </div>
-                    )}
-                    </Spring>
+                        </Spring>
+                    </div>
                 </div>
             </div>
         </div>
