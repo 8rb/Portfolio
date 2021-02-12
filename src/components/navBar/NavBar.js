a3614b400e4a37df2b11354c5a5cb263662dfc4a
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-02-11 21:30:14 -0500

    ⚡️ Active Links NavBar
    Added Active links Underline for navbar.

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import './Styles.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Toggle from '../toggle/Toggle';
 
 const NavBar = () => {
     return(
-        <div className="navbar-container navBar">
-            <div className="navbar-row">
+        <>
+            <div className="toggle">
                 <Toggle/>
             </div>
-            <div className="navbar-row link-row">
-                <div className="navbar-col">
-                    <Link className="navLink" to="/">Home</Link>
-                </div>
-                <div className="navbar-col">
-                    <Link className="navLink" to="/projects">Projects</Link>
+            <div className="navbar-container navBar">
+                <div className="navbar-row link-row">
+                    <div className="navbar-col">
+                        <NavLink exact={true} activeClassName='activeLink' className="navLink" to="/">
+                            <p className="navText">Home</p>
+                            <hr/>
+                        </NavLink>
+                    </div>
+                    <div className="navbar-col">
+                        <NavLink activeClassName='activeLink' className="navLink" to="/projects">
+                            <p className="navText">Projects</p>
+                            <hr/>
+                        </NavLink>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 export default NavBar;
