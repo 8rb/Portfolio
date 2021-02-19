33eac841630d1060d5ebcbc8404a02fdb9294e9c
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-02-18 21:13:44 -0500

    ⚡️ Persisted State on reload
    Persisted Redux Store on Page Reload/Refresh using redux-persist.

---
+++ src/redux/themeReducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+    theme: 'dark',
+    loading: true
+}
+
+export default function themeReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'toggleTheme':
+            if(state.theme === 'light'){
+                return { ...state, theme: 'dark' }
+            } 
+            else {
+                return { ...state, theme: 'light' }
+            }
+        case 'stopLoading':
+            if(state.loading)
+                return { ...state, loading: false}
+            else 
+                return state;
+      default:
+            return state;
+    }
+}
