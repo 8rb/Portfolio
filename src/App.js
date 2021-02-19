9abeb5a3b9df91070626ade71dbc75fd0433ac09
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-02-18 21:14:17 -0500

    ✨ Loading Screen
    Added initial loading screen on initial load.

--- src/App.js
+++ src/App.js
@@ -5,39 +5,60 @@ import Projects from './components/projects/Projects';
 import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Contact from './components/contactPage/Contact';
-import store from './redux/store';
+import { store } from './redux/store';
 import { connect } from 'react-redux'
 
-const App = () => {
+class App extends React.Component {
+  state = {
+    loading: true,
+  };
 
-  return(
-    <div className={'theme ' + store.getState().theme}>
-      <Router>
-        <div className="navbar-content">
-          <NavBar/>
-        </div>
-        <div className="main-content">
-          <Switch>
-            <Route exact path="/">
-                <Home/>
-            </Route>
-            <Route path="/projects">
-                <Projects/>
-            </Route>
-            <Route path="/contact">
-                <Contact/>
-            </Route>
-            <Route path="*">
-                <Redirect to="/"/>
-            </Route>
-          </Switch>
-        </div>
-        <div className="footer-content">
-          <Footer/>
-        </div>
-      </Router>
-    </div>
-  );
+  componentDidMount() {
+    // this simulates an async action, after which the component will render the content
+    demoAsyncCall().then(() => this.setState({ loading: false }));
+  }
+
+  render() {
+    return(
+      <>
+      {this.state.loading &&
+      <div className={`loadingScreen`}>
+        <div className="lds-ripple"><div></div><div></div></div>
+      </div>
+      }
+      <div className={`theme ${store.getState().theme}`}>
+        <Router>
+          <div className="navbar-content">
+            <NavBar/>
+          </div>
+          <div className="main-content">
+            <Switch>
+              <Route exact path="/">
+                  <Home/>
+              </Route>
+              <Route path="/projects">
+                  <Projects/>
+              </Route>
+              <Route path="/contact">
+                  <Contact/>
+              </Route>
+              <Route path="*">
+                  <Redirect to="/"/>
+              </Route>
+            </Switch>
+          </div>
+          <div className="footer-content">
+            <Footer/>
+          </div>
+        </Router>
+      </div>
+      </>
+    );
+  }
+}
+
+function demoAsyncCall() {
+  return new Promise((resolve) => setTimeout(() => resolve(), 2000));
 }
 
 const mapStateToProps = (state) => {
