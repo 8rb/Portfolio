bea52c53b8794bb0b9e08a308c381a7cdbed5dcc
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-02-20 20:47:04 -0500

    🔨 Refactored Nav and Footer
    Refactored navbar and footer (navMenu) for new design 🎨.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import ContactNav from '../contactNav/ContactNav';
 import NavMenu from '../navMenu/NavMenu';
 import './Styles.scss';
+
 const Footer = () => {
     return (
         <div className="footer-container">
-                <ContactNav/>
-                <NavMenu/>
+            <NavMenu/>
         </div>
     )
 }
