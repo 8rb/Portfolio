f6e1c1ca22178f8ecd1d36a484ece03906a949e8
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-02-20 20:47:04 -0500

    🔨 Refactored Nav and Footer
    Refactored navbar and footer (navMenu) for new design 🎨.

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import './Styles.scss';
 import { NavLink } from 'react-router-dom';
-import Toggle from '../toggle/Toggle';
+// import Toggle from '../toggle/Toggle';
 
 const NavBar = () => {
     return(
         <>
-            <div className="toggle">
+            {/* <div className="toggle">
                 <Toggle/>
-            </div>
+            </div> */}
             <div className="navbar-container navBar">
                 <div className="navbar-row link-row">
                     <div className="navbar-col">
                         <NavLink exact={true} activeClassName='activeLink' className="navLink" to="/">
                             <p className="navText">Home</p>
-                            <hr/>
                         </NavLink>
                     </div>
                     <div className="navbar-col">
                         <NavLink activeClassName='activeLink' className="navLink" to="/projects">
                             <p className="navText">Projects</p>
-                            <hr/>
                         </NavLink>
                     </div>
                 </div>
