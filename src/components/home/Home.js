e3f063bf9819bc4833da208539e65800e4c8ab4a
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-02-21 21:27:54 -0500

    ⚡️ Updated Home
    Updated Home and started animations.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import {useSpring, animated} from 'react-spring';
-import './Styles.scss';
+import './Home.scss';
 
 const Home = () => {
-    const props = useSpring({
+    const Nameprops = useSpring({
         opacity: 1, 
         from: {opacity: 0},
-        config: {duration: 1500}
+        config: {duration: 500}
     })
     return(
         <div className="home">
             <div className="name-row">
-                <animated.div className="name-col" style={props}>
+                <animated.div className="name-col" style={Nameprops}>
                     <h1 className="name">RODRIGO</h1>
                     <h1 className="name">RAMIREZ</h1>
                 </animated.div>
             </div>
             <div className="job-row">
-                <animated.div className="job-col" style={props}>
+                <div className="job-col">
                     <h1 className="job">Front End Developer</h1>
-                </animated.div>  
+                </div>
             </div>
         </div>
     );
