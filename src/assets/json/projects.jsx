24cbc617bde1aad03e05c57e65bfc7d58a5a3080
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-02-24 21:32:45 -0500

    📝 Updated Projects array
    Updated projects array: changed id, added generic_name, updated img url, and changed tags.

--- src/assets/json/projects.jsx
+++ src/assets/json/projects.jsx
@@ -2,36 +2,39 @@ import React from 'react';
 
 export const projects = [
     {
-        "id": 1,
+        "id": 0,
         "title": "PokeApp",
+        "generic_name": "POKEMON APP",
         "description": <>PokeApp is a <b className="bold">Pokemon Search Engine</b> and type calculator developed in React. It fetches data from an external API (PokeAPI) using Axios and filters the most accurate results based on the user input.</>,
         "github_link": "https://github.com/8rb/PokeApp",
         "deployed_link": "https://pokeapp.onrender.com/search",
-        "img": require("../images/PokeApp.jpg"),
+        "img": require("../images/PokeApp.png"),
         "tags": [
-            "React", "·", "Typescript", "·", "API"
+            "Search Engine", "Input Prediction", "PWA"
+        ]
+    },
+    {
+        "id": 1,
+        "title": "Bodega",
+        "generic_name": "ONLINE STORE",
+        "description": <>Bodega is an online <b className="bold">shopping website</b>, based on Peruvian 'Bodegas'. It was developed in Vue, and uses Bootstrap and SCSS for the UI elements. This project uses Vuex store to store the <b className="bold">state of the cart</b> when rendering the component.</>,
+        "github_link": "https://github.com/8rb/Bodega",
+        "deployed_link": "https://bodega.onrender.com/store",
+        "img": require("../images/Bodega.png"),
+        "tags": [
+            "Vue", "Vuex","Bootstrap"
         ]
     },
     {
         "id": 2,
-        "title": "Bodega",
-        "description": <>Bodega is an online <b className="bold">shopping website</b>, based on Peruvian 'Bodegas'. It was developed in Vue, and uses Bootstrap and SCSS for the UI elements. This project uses Vuex store to store the <b className="bold">state of the cart</b> when rendering the component.</>,
-        "github_link": "https://github.com/8rb/Bodega",
-        "deployed_link": "https://bodega.onrender.com/store",
-        "img": require("../images/Bodega.jpg"),
-        "tags": [
-            "Vue", "·", "Vuex", "·","Bootstrap"
-        ]
-    },
-    {
-        "id": 3,
         "title": "Interlab",
+        "generic_name": "INTERNSHIP FINDER",
         "description": <>Interlab is an <b className="bold">internship applications platform</b>, developed in Angular + TypeScript in the Front-End and Spring in the backend. I was in charge of the Majority of the Front-End programming and the database design. This application accesses the backend using Axios for HTTP requests.</>,
         "github_link": "https://github.com/8rb/interlab-webapps",
         "deployed_link": "https://interlab-2fmj.onrender.com",
-        "img": require("../images/InterLab.jpg"),
+        "img": require("../images/Interlab.png"),
         "tags": [
-            "Angular", "·", "TypeScript"
+            "Angular", "TypeScript", "CSS"
         ]
     },
         
