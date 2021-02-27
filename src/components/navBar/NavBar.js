30d15dd582f5b1449b069414a488887582855255
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-02-26 20:53:19 -0500

    🎨 Updated navbar
    Changed hover animation of navbar links.

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import './Styles.scss';
+import './NavBar.scss';
 import { NavLink } from 'react-router-dom';
-// import Toggle from '../toggle/Toggle';
 
 const NavBar = () => {
     return(
         <>
-            {/* <div className="toggle">
-                <Toggle/>
-            </div> */}
             <div className="navbar-container navBar">
                 <div className="navbar-row link-row">
                     <div className="navbar-col">
-                        <NavLink exact={true} activeClassName='activeLink' className="navLink" to="/">
+                        <NavLink exact={true} activeClassName='navBarActiveLink' className="navLink" to="/">
                             <p className="navText">Home</p>
+                            <hr/>
                         </NavLink>
                     </div>
                     <div className="navbar-col">
-                        <NavLink activeClassName='activeLink' className="navLink" to="/projects">
+                        <NavLink activeClassName='navBarActiveLink' className="navLink" to="/projects">
                             <p className="navText">Projects</p>
+                            <hr/>
                         </NavLink>
                     </div>
                     <div className="navbar-col">
-                        <NavLink activeClassName='activeLink' className="navLink" to="/contact">
+                        <NavLink activeClassName='navBarActiveLink' className="navLink" to="/contact">
                             <p className="navText">Contact</p>
+                            <hr/>
                         </NavLink>
                     </div>
                 </div>
