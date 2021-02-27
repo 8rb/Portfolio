1aa3dce67b0a5746cb7bd1d85dfd9e285dc2e183
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-02-27 14:07:06 -0500

    ⚡️ New Project Component
    Created new project component using scss modules.

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,41 +1,39 @@
 import React from 'react';
 import projectsInfo from '../../assets/json/projects.jsx';
-import './Styles.scss';
+import styles from './Project.module.scss';
 
 const Project = ({index}) => {
+  
   return (
-    <div className="pro-container project">
-      <div className="pro-row title-row">
-        <div className="pro-col title-col">
-          <h1 className="titleP">{projectsInfo[index].title}</h1>
-        </div>
-      </div>
-      <div className="pro-row info-row">
-        <div className="pro-col image-col">
-          <a className="image-link" href={projectsInfo[index].deployed_link} target="_blank" rel="noopener noreferrer">
-            <img className="imageP" src={projectsInfo[index].img} alt="project"/>
-          </a>
-        </div>
-        <div className="pro-col info-col">
-          <div className="tags-row text-col">
-            {projectsInfo[index].tags.map((tag, i) =>
-              <div className="tag-div" key={i}><p className="tag">{tag}</p></div>
-            )}
-          </div>
-          <div className="desc-row">
-            <p className="text">{projectsInfo[index].description}</p>
-          </div>
-        </div>
-      </div>
-      <div className="links-row">
-        <div className="link-col left-col">
-          <a className="btn github" href={projectsInfo[index].github_link} target="_blank" rel="noopener noreferrer">Github Repo</a>
-        </div>
-        <div className="link-col">
-          <a className="btn live" href={projectsInfo[index].deployed_link} target="_blank" rel="noopener noreferrer">Live Site</a>
-        </div>
+    <>
+    <div className={styles.titleWrapper}>
+      <div className={styles.titleRow}>
+        <h1 className={styles.title}>{projectsInfo[index].generic_name}</h1>
       </div>
     </div>
+    <div className={styles.projectWrapper}>
+      {/* <div className={styles.imageRow}>
+        <img className={styles.image} src={projectsInfo[index].img} alt={projectsInfo[index].generic_name}/>
+      </div> */}
+      <div className={styles.aboutRow}>
+        {/* <div className={styles.underlineRow}>
+          <h1 className={styles.subtitle}>ABOUT</h1>
+        </div> */}
+        <div className={styles.aboutInfo}>
+          <p className={styles.aboutText}>{projectsInfo[index].description}</p>
+        </div>
+      </div>
+      <div className={styles.linksRow}>
+        <div className={styles.underlineRow}>
+          <h1 className={styles.subtitle}>LINKS</h1>
+        </div>
+        <div className={styles.buttonsGrid}>
+          <a className={styles.githubBtn} href={projectsInfo[index].github_link} target="_blank" rel="noopener noreferrer">Github Repo</a>
+          <a className={styles.siteBtn} href={projectsInfo[index].deployed_link} target="_blank" rel="noopener noreferrer">Live Site</a>
+        </div>
+       </div>
+    </div>
+    </>
   );
 }
 
