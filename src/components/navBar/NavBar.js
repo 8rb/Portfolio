f2a30e52d78269c57e4bb3510482c9a4f346642f
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-03-04 19:11:25 -0500

    🎨 Minor Styling Fixes
    Change to a darker background, changed job styles in Home Component, Changed Navbar links styles.

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -9,19 +9,19 @@ const NavBar = () => {
                 <div className="navbar-row link-row">
                     <div className="navbar-col">
                         <NavLink exact={true} activeClassName='navBarActiveLink' className="navLink" to="/">
-                            <p className="navText">Home</p>
+                            <p className="navText">HOME</p>
                             <hr/>
                         </NavLink>
                     </div>
                     <div className="navbar-col">
                         <NavLink activeClassName='navBarActiveLink' className="navLink" to="/projects">
-                            <p className="navText">Projects</p>
+                            <p className="navText">PROJECTS</p>
                             <hr/>
                         </NavLink>
                     </div>
                     <div className="navbar-col">
                         <NavLink activeClassName='navBarActiveLink' className="navLink" to="/contact">
-                            <p className="navText">Contact</p>
+                            <p className="navText">CONTACT</p>
                             <hr/>
                         </NavLink>
                     </div>
