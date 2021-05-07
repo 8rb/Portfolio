419a2237e0bf739bd38b1d8ea407a522b3958f22
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-05-07 10:53:12 -0500

    🔨 Implement Interface Button
    Implement JSX interface for react-awesome-button

---
+++ src/components/button/Social.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AwesomeButtonSocial } from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
+import './button.scss';
+
+const SocialButton = ({type, text, href ="", target ="", onPress=() => {}}) => {
+    return (
+        <>
+        <AwesomeButtonSocial type={type} href={href} target={target}
+        noopener noreferrer onPress={onPress}>{text}</AwesomeButtonSocial>
+        </>
+    )
+}
+
+export default SocialButton
