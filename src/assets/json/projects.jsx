4f1180f6c2959b2506819a94fe6c1ae2baff4d79
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-08-31 18:27:23 -0500

    🔗 Update Links
    Update Github, LinkedIn, and project links.

--- src/assets/json/projects.jsx
+++ src/assets/json/projects.jsx
@@ -1,61 +1,75 @@
-import React from 'react';
+import React from 'react'
 
 export const projects = [
-    {
-        "id": 0,
-        "title": "PokeApp",
-        "generic_name": "POKEMON APP",
-        "description": <>PokeApp is a <b className="bold">Pokemon Search Engine</b> and type calculator developed in React. It fetches data from an external API (PokeAPI) using Axios and filters the most accurate results based on the user input.</>,
-        "github_link": "https://github.com/8rb/PokeApp",
-        "deployed_link": "https://pokeapp.onrender.com/search",
-        "img": require("../images/PokeApp.webp"),
-        "tags": [
-            "Search Engine", "Input Prediction", "PWA"
-        ]
-    },
-    {
-        "id": 1,
-        "title": "Bodega",
-        "generic_name": "ONLINE STORE",
-        "description": <>Bodega is an online <b className="bold">shopping website</b>, based on Peruvian 'Bodegas'. It was developed in Vue, and uses Bootstrap and SCSS for the UI elements. This project uses Vuex store to store the <b className="bold">state of the cart</b> when rendering the component.</>,
-        "github_link": "https://github.com/8rb/Bodega",
-        "deployed_link": "https://bodega.onrender.com/store",
-        "img": require("../images/Bodega.webp"),
-        "tags": [
-            "Vue", "Vuex","Bootstrap"
-        ]
-    },
-    {
-        "id": 2,
-        "title": "Interlab",
-        "generic_name": "INTERNSHIP FINDER",
-        "description": <>Interlab is an <b className="bold">internship applications platform</b>, developed in Angular + TypeScript in the Front-End and Spring in the backend. I was in charge of the Majority of the Front-End programming and the database design. This application accesses the backend using Axios for HTTP requests.</>,
-        "github_link": "https://github.com/Juanca0312/interlab",
-        "deployed_link": "https://interlab-2fmj.onrender.com",
-        "img": require("../images/Interlab.webp"),
-        "tags": [
-            "Angular", "TypeScript", "CSS"
-        ]
-    },
-        
-        // {
-        //     "id": 4,
-        //     "title": "Algorite",
-        //     "description": "",
-        //     "github_link": "",
-        //     "deployed_link": "",
-        //     "img": "",
-        //     "img_classname": ""
-        // },
-        // {
-        //     "id": 5,
-        //     "title": "Quizify",
-        //     "description": "",
-        //     "github_link": "",
-        //     "deployed_link": "",
-        //     "img": "",
-        //     "img_classname": ""
-        // }
-    ]
+	{
+		id: 0,
+		title: 'PokeApp',
+		generic_name: 'POKEMON APP',
+		description: (
+			<>
+				PokeApp is a <b className='bold'>Pokemon Search Engine</b> and type
+				calculator developed in React. It fetches data from an external API
+				(PokeAPI) using Axios and filters the most accurate results based on the
+				user input.
+			</>
+		),
+		github_link: 'https://github.com/TheRoro/PokeApp',
+		deployed_link: 'https://pokeapp.onrender.com/search',
+		img: require('../images/PokeApp.webp'),
+		tags: ['Search Engine', 'Input Prediction', 'PWA'],
+	},
+	{
+		id: 1,
+		title: 'Bodega',
+		generic_name: 'ONLINE STORE',
+		description: (
+			<>
+				Bodega is an online <b className='bold'>shopping website</b>, based on
+				Peruvian 'Bodegas'. It was developed in Vue, and uses Bootstrap and SCSS
+				for the UI elements. This project uses Vuex store to store the{' '}
+				<b className='bold'>state of the cart</b> when rendering the component.
+			</>
+		),
+		github_link: 'https://github.com/TheRoro/Bodega',
+		deployed_link: 'https://bodega.onrender.com/store',
+		img: require('../images/Bodega.webp'),
+		tags: ['Vue', 'Vuex', 'Bootstrap'],
+	},
+	{
+		id: 2,
+		title: 'UpSpell',
+		generic_name: 'SPECIAL CHARACTERS',
+		description: (
+			<>
+				<b className='bold'>UpSpell</b>, is a Special Characters Web Application
+				developed in Vue/Nuxt whose main objective is to provide special
+				characters for all those who are learning a language or whose keymap
+				does not have the character they want to type.
+			</>
+		),
+		github_link: 'https://github.com/TheRoro/UpSpell',
+		deployed_link: 'https://upspell.vercel.app/',
+		img: require('../images/UpSpell.webp'),
+		tags: ['Nuxt', 'Vue', 'TailwindCSS'],
+	},
+	// {
+	// 	id: 4,
+	// 	title: 'Algorite',
+	// 	description: '',
+	// 	github_link: '',
+	// 	deployed_link: '',
+	// 	img: '',
+	// 	img_classname: '',
+	// },
+	// {
+	// 	id: 5,
+	// 	title: 'Quizify',
+	// 	description: '',
+	// 	github_link: '',
+	// 	deployed_link: '',
+	// 	img: '',
+	// 	img_classname: '',
+	// },
+]
 
-export default projects;
+export default projects
