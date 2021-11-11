b2d7e1ef0de813ec0a9715861dc20f3ac528580a
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-11-11 08:40:33 -0500

    🎉Initialize Gatsby Project

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-smoothscroll`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+    },
+  ],
+}
