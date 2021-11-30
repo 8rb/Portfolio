d8bcbd1ba87cb2cd4112ecec68125ac5f1cbecca
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-11-30 08:26:08 -0500

    ✨Additional Components
    Add Card and footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./styles.scss"
+
+function Footer() {
+  return (
+    <div className="footer" id="footer">
+      <p>
+        <span>Designed & Developed by</span>{" "}
+        <span className="name">Rodrigo Ramirez</span>
+      </p>
+    </div>
+  )
+}
+
+export default Footer
