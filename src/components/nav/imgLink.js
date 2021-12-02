459b46562ee1b53f7d1e300cc2eab9753e67de00
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-12-02 10:00:36 -0500

    🐛Fix Scroll Link Image

---
+++ src/components/nav/imgLink.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link as LinkScroll } from "react-scroll"
+
+function ImageLink({ href, img }) {
+  return (
+    <LinkScroll
+      activeClass="active"
+      to={href}
+      spy={true}
+      smooth={true}
+      duration={1000}
+    >
+      {img}
+    </LinkScroll>
+  )
+}
+
+export default ImageLink
