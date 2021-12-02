29c5a381a1a6927f921a23e911aba39a1dffa680
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-12-02 10:00:36 -0500

    🐛Fix Scroll Link Image

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,18 +1,23 @@
 import React from "react"
 import "./styles.scss"
 import Link from "./link"
+import ImageLink from "./imgLink"
 import { StaticImage } from "gatsby-plugin-image"
-import scrollTo from "gatsby-plugin-smoothscroll"
 
 function Nav() {
   return (
     <div className="nav">
-      <StaticImage
-        src="../../../static/static_logo.png"
-        className="nav-logo"
-        alt="main logo"
-        backgroundColor="transparent"
-        onClick={() => scrollTo("#home")}
+      <ImageLink
+        href="home"
+        img={
+          <StaticImage
+            src="../../../static/static_logo.png"
+            className="nav-logo"
+            alt="main logo"
+            backgroundColor="transparent"
+            placeholder="none"
+          />
+        }
       />
       <div className="links">
         <Link href="projects" name="projects" />
