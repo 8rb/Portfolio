4d6b97b0ecd0859c8da3f1c1870ff03be0d039a9
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-12-03 08:14:46 -0500

    ⬆️ Update Dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,21 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Rodrigo Ramirez",
+    titleTemplate: "%s · Web Developer Portfolio",
+    description:
+      "Web Development Portfolio of Rodrigo Ramirez. Front End Developer specialized in Single Page Application technologies such as: React and Vue.",
+    url: "https://rodrigoramirez.dev", // No trailing slash allowed!
+    image: "/static_logo.png", // Path to the image placed in the 'static' folder, in the project's root directory.
+  },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-smoothscroll`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: "gatsby-plugin-react-svg",
     },
