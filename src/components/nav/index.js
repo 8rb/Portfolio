4982d54911677da4d4bf94474f969b275b0f5859
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-12-11 13:12:38 -0500

    ⚡️Update Nav
    Add sphere as home logo

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -3,22 +3,12 @@ import "./styles.scss"
 import Link from "./link"
 import ImageLink from "./imgLink"
 import { StaticImage } from "gatsby-plugin-image"
+import Sphere from "../../pages/sphere"
 
 function Nav() {
   return (
     <div className="nav">
-      <ImageLink
-        href="home"
-        img={
-          <StaticImage
-            src="../../../static/static_logo.png"
-            className="nav-logo"
-            alt="main logo"
-            backgroundColor="transparent"
-            placeholder="none"
-          />
-        }
-      />
+      <ImageLink href="home" img={<Sphere className="brand-sphere" />} />
       <div className="links">
         <Link href="projects" name="projects" />
         <Link href="about" name="about" />
