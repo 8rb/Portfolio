9e7323468f7f65a8e6b2316f5147b824d0b16c8b
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-12-30 09:40:46 -0500

    📝 Update Projects Information
    Update description, add a color palette and add portfolio project.

--- src/components/projectsInfo.js
+++ src/components/projectsInfo.js
@@ -4,8 +4,15 @@ const bodega = {
   keywords: "E-commerce",
   summary: "Online shopping web app based on peruvian “bodegas”.",
   description:
-    "Online shopping web app based on peruvian “bodegas”. Online shopping web app based on peruvian “bodegas”. Online shopping web app based on peruvian “bodegas”. Online shopping web app based on peruvian “bodegas”. Online shopping web app based on peruvian “bodegas”.",
+    "Bodega is an online shopping website, based on Peruvian 'Bodegas'. It was developed in Vue, and uses Bootstrap and SCSS for the UI elements. This project uses Vuex store to store the state of the cart when rendering the component.",
   tags: "Vue Vuex Sass",
+  palette: [
+    { background: "#FAFAFA", text: "#353535" },
+    { background: "#FF6699", text: "#FFFFFF" },
+    { background: "#1A1A1A", text: "#FFFFFF" },
+    { background: "#777777", text: "#FFFFFF" },
+    { background: "#FFA5C3", text: "#000000" },
+  ],
   imgUrl: "bodega.png",
   repoUrl: "https://github.com/TheRoro/Bodega",
   webUrl: "https://bodega.onrender.com/shop",
@@ -14,11 +21,18 @@ const bodega = {
 const pokeapp = {
   title: "PokeApp",
   name: "pokeapp",
-  keywords: "Pokemon Search",
+  keywords: "Search Engine",
   summary: "Pokemon search engine and PWA using PokeAPI.",
   description:
-    "Pokemon search engine and PWA using PokeAPI. Pokemon search engine and PWA using PokeAPI. Pokemon search engine and PWA using PokeAPI. Pokemon search engine and PWA using PokeAPI.",
+    "PokeApp is a Pokemon Search Engine and type calculator developed in React. It fetches data from an external API (PokeAPI) using Axios and filters the most accurate results based on the user input.",
   tags: "React TypeScript Bootstrap",
+  palette: [
+    { background: "#121212", text: "#FFFFFF" },
+    { background: "#333333", text: "#FFFFFF" },
+    { background: "#eb1b24", text: "#FFFFFF" },
+    { background: "#FAFAFA", text: "#353535" },
+    { background: "#666666", text: "#FFFFFF" },
+  ],
   imgUrl: "pokeapp.png",
   repoUrl: "https://github.com/TheRoro/PokeApp",
   webUrl: "https://pokeap.netlify.app/search",
@@ -30,11 +44,38 @@ const upspell = {
   keywords: "Special Characters",
   summary: "Special characters web app for different languages.",
   description:
-    "Special characters web app for different languages. Special characters web app for different languages. Special characters web app for different languages.",
+    "UpSpell, is a Special Characters Web Application developed in Vue/Nuxt whose main objective is to provide special characters for all those who are learning a language or whose keymap does not have the character they want to type.",
   tags: "Nuxt Tailwind",
+  palette: [
+    { background: "#3b82f6", text: "#FFFFFF" },
+    { background: "#fcd34d", text: "#000000" },
+    { background: "#2a2a29", text: "#FFFFFF" },
+    { background: "#ffffff", text: "#353535" },
+    { background: "#F59E0B", text: "#000000" },
+  ],
   imgUrl: "upspell.png",
   repoUrl: "https://github.com/TheRoro/UpSpell",
   webUrl: "https://upspell.vercel.app/",
 }
 
-export { bodega, pokeapp, upspell }
+const portfolio = {
+  title: "Front End Portfolio",
+  name: "portfolio",
+  keywords: "this.portfolio",
+  summary: "Front End Web development Porfolio.",
+  description:
+    "Front End Web development Porfolio developed using React, Gatsby and a handful of animation libraries like React Three Fiber, React Spring and Framer Motion to add interaction to the website.",
+  tags: "Gatsby Framer Motion",
+  palette: [
+    { background: "#16ffd8", text: "#353535" },
+    { background: "#121921", text: "#FFFFFF" },
+    { background: "#d4ddf8", text: "#000000" },
+    { background: "#8980f5", text: "#FFFFFF" },
+    { background: "#ff7979", text: "#FFFFFF" },
+  ],
+  imgUrl: "portfolio.png",
+  repoUrl: "https://github.com/TheRoro/Portfolio",
+  webUrl: "https://rodrigoramirez.dev/",
+}
+
+export { bodega, pokeapp, upspell, portfolio }
