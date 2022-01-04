3a55a9740eaa3c5cef502be0f74dbb7ef127192b
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-01-04 08:33:28 -0500

    ⚡️Add Portfolio Project

--- src/modules/allProjects/index.js
+++ src/modules/allProjects/index.js
@@ -1,16 +1,22 @@
 import React from "react"
 import ProjectTile from "../../components/projectTile"
-import { bodega, pokeapp, upspell } from "../../components/projectsInfo"
+import {
+  bodega,
+  pokeapp,
+  upspell,
+  portfolio,
+} from "../../components/projectsInfo"
 import "./styles.scss"
 
 const ProjectsModule = () => {
   return (
-    <div className="projects">
+    <section className="projects">
       <h1 className="section-title mb-5">All Projects</h1>
       <ProjectTile project={bodega} orientation="right" />
       <ProjectTile project={pokeapp} orientation="left" />
       <ProjectTile project={upspell} orientation="right" />
-    </div>
+      <ProjectTile project={portfolio} orientation="left" />
+    </section>
   )
 }
 
